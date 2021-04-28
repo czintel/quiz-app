564295b49dcc85d965df6ec9ffd665d1e457c8b8
Christian Zintel <c.zintel@yahoo.de>
2021-04-28 09:34:37 +0200

    created JS files and made main-sectors toggleable

---
+++ src/index.js
@@ -0,0 +1,66 @@
+console.clear()
+
+const answerHide1 = document.querySelector('.js-answer1')
+const buttonHide1 = document.querySelector('.js-button-hide1')
+
+buttonHide1.addEventListener('click', () => {
+  answerHide1.classList.toggle('hidden')
+  buttonHide1.classList.toggle('mb30')
+})
+
+const mainHome = document.querySelector('.js-main-home')
+const mainBookmark = document.querySelector('.js-main-bookmark')
+const mainCreate = document.querySelector('.js-main-create')
+const mainProfile = document.querySelector('.js-main-profile')
+const buttonHome = document.querySelector('.js-button-home')
+const buttonBookmark = document.querySelector('.js-button-bookmark')
+const buttonCreate = document.querySelector('.js-button-create')
+const buttonProfile = document.querySelector('.js-button-profile')
+const headerHome = document.querySelector('.js-header-home')
+const headerBookmark = document.querySelector('.js-header-bookmark')
+const headerCreate = document.querySelector('.js-header-create')
+const headerProfile = document.querySelector('.js-header-profile')
+
+buttonHome.addEventListener('click', () => {
+  mainHome.classList.remove('hidden')
+  headerHome.classList.remove('hidden')
+  mainBookmark.classList.add('hidden')
+  headerBookmark.classList.add('hidden')
+  mainCreate.classList.add('hidden')
+  headerCreate.classList.add('hidden')
+  mainProfile.classList.add('hidden')
+  headerProfile.classList.add('hidden')
+})
+
+buttonBookmark.addEventListener('click', () => {
+  mainHome.classList.add('hidden')
+  headerHome.classList.add('hidden')
+  mainBookmark.classList.remove('hidden')
+  headerBookmark.classList.remove('hidden')
+  mainCreate.classList.add('hidden')
+  headerCreate.classList.add('hidden')
+  mainProfile.classList.add('hidden')
+  headerProfile.classList.add('hidden')
+})
+
+buttonCreate.addEventListener('click', () => {
+  mainHome.classList.add('hidden')
+  headerHome.classList.add('hidden')
+  mainBookmark.classList.add('hidden')
+  headerBookmark.classList.add('hidden')
+  mainCreate.classList.remove('hidden')
+  headerCreate.classList.remove('hidden')
+  mainProfile.classList.add('hidden')
+  headerProfile.classList.add('hidden')
+})
+
+buttonProfile.addEventListener('click', () => {
+  mainHome.classList.add('hidden')
+  headerHome.classList.add('hidden')
+  mainBookmark.classList.add('hidden')
+  headerBookmark.classList.add('hidden')
+  mainCreate.classList.add('hidden')
+  headerCreate.classList.add('hidden')
+  mainProfile.classList.remove('hidden')
+  headerProfile.classList.remove('hidden')
+})
